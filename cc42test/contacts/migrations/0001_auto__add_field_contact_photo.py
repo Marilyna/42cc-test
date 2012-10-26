c2feedc94cb94085c488d95ebc487c57a8e8aa2e
Marina Solovyova <marinka.solovyova@gmail.com>
2012-10-26 21:31:52 +0300

    ticket:5 add migration

---
+++ cc42test/contacts/migrations/0001_auto__add_field_contact_photo.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Contact.photo'
+        db.add_column('contacts_contact', 'photo',
+                      self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Contact.photo'
+        db.delete_column('contacts_contact', 'photo')
+
+
+    models = {
+        'contacts.contact': {
+            'Meta': {'object_name': 'Contact'},
+            'bio': ('django.db.models.fields.TextField', [], {}),
+            'birth_date': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'contacts.request': {
+            'Meta': {'object_name': 'Request'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'method': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['contacts']
