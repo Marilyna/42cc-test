30747d2290cb1ddcc66df9f166ad84e8ae4fbf03
Marina Solovyova <marinka.solovyova@gmail.com>
2012-10-27 13:42:59 +0300

    ticket:5 add initial migration

---
+++ cc42test/contacts/migrations/0001_initial.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Contact'
+        db.create_table('contacts_contact', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=20)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=20)),
+            ('birth_date', self.gf('django.db.models.fields.DateField')()),
+            ('bio', self.gf('django.db.models.fields.TextField')()),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('jabber', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('skype', self.gf('django.db.models.fields.CharField')(max_length=20)),
+            ('other_contacts', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal('contacts', ['Contact'])
+
+        # Adding model 'Request'
+        db.create_table('contacts_request', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('method', self.gf('django.db.models.fields.CharField')(max_length=10)),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal('contacts', ['Request'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Contact'
+        db.delete_table('contacts_contact')
+
+        # Deleting model 'Request'
+        db.delete_table('contacts_request')
+
+
+    models = {
+        'contacts.contact': {
+            'Meta': {'object_name': 'Contact'},
+            'bio': ('django.db.models.fields.TextField', [], {}),
+            'birth_date': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'other_contacts': ('django.db.models.fields.TextField', [], {}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'contacts.request': {
+            'Meta': {'object_name': 'Request'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'method': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['contacts']
