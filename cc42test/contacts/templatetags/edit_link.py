70d0e9654b2a3bdd9e0408a2b4df034175e4a30e
Marina Solovyova <marinka.solovyova@gmail.com>
2012-10-27 21:29:24 +0300

    ticket:8 working template tag

--- cc42test/contacts/templatetags/edit_link.py
+++ cc42test/contacts/templatetags/edit_link.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.urlresolvers import reverse
 
 
 register = template.Library()
@@ -6,4 +7,6 @@ register = template.Library()
 
 @register.simple_tag
 def edit_link(instance):
-    return ''
+    info = instance._meta.app_label, instance._meta.module_name
+    name = 'admin:%s_%s_change' % info
+    return reverse(name, args=[instance.pk])
