0273d2e32489a3ce02119015bf000021e8adb801
Marina Solovyova <marinka.solovyova@gmail.com>
2012-10-29 18:26:18 +0200

    ticket:10 implementation for ModelLog

--- cc42test/contacts/models.py
+++ cc42test/contacts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save, post_delete
 from django.contrib.contenttypes.models import ContentType
 
 
@@ -33,3 +34,22 @@ class ModelLog(models.Model):
     content_type = models.ForeignKey(ContentType, related_name='+')
     action = models.CharField(max_length=1, choices=CUD)
     timestamp = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def record(cls, sender, action):
+        if sender == cls:
+            return
+        ctype = ContentType.objects.get_for_model(sender)
+        cls.objects.create(content_type=ctype, action=action)
+
+    @classmethod
+    def post_save(cls, sender, created, **kwargs):
+        cls.record(sender, 'C' if created else 'U')
+
+    @classmethod
+    def post_delete(cls, sender, **kwargs):
+        cls.record(sender, 'D')
+
+
+post_save.connect(ModelLog.post_save)
+post_delete.connect(ModelLog.post_delete)
